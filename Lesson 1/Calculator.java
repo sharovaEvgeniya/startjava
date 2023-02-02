fcc3d78500849913f2ab4419834f1bda7829612d
EvgeniyaSharova <sharova.evgeniya8@gmail.com>
2023-02-02 12:51:28 +0300

    chek

---
+++ Lesson 1/Calculator.java
@@ -0,0 +1,28 @@
+public class Calculator {
+
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 3;
+        char sign = '^';
+        int result = 1;
+
+        if(sign == '+') {
+            result = a + b;
+        } else if(sign == '-') {
+            result = a - b;
+        } else if(sign == '*') {
+            result = a * b;
+        } else if(sign == '/') {
+            result = a / b;
+        } else if(sign == '%') {
+            result = a % b;
+        } else if(sign == '^') {
+            for(int i = 0; i < b; i++) {
+                result *= a;
+                System.out.println(result);
+            }
+        }
+        
+        System.out.println(a + " " + sign + " " + b + " = " + result);
+    }
+}
