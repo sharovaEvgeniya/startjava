a55a1f10700fc0d34a2f7c7515afba516799b10d
EvgeniyaSharova <sharova.evgeniya8@gmail.com>
2023-02-07 15:06:02 +0300

    chek

---
+++ Lesson 2/Person.java
@@ -0,0 +1,27 @@
+public class Person {
+    String gender = "woman";
+    String name = "Evgenia";
+    int height = 168;
+    int weight = 57;
+    int age = 26;
+
+    void toGo() {
+        System.out.println("иди");
+    }
+
+    void toSit() {
+        System.out.println("сиди");
+    }
+
+    void toRun() {
+        System.out.println("беги");
+    }
+
+    void toSay() {
+        System.out.println("говори");
+    }
+
+    void learnJava() {
+        System.out.println("учи Java");
+    }
+}
