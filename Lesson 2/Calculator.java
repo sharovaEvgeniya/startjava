1743f42ee815f0ecd39e09ef6f0ccb664e3f8d67
EvgeniyaSharova <sharova.evgeniya8@gmail.com>
2023-02-13 17:04:25 +0300

    chek

---
+++ Lesson 2/Calculator.java
@@ -0,0 +1,28 @@
+public class Calculator {
+    public void calculator(int num1, char sign, int num2) {
+        int result = 1;
+        switch (sign) {
+            case '+':
+                result = num1 + num2;
+                break;
+            case '-':
+                result = num1 - num2;
+                break;
+            case '*':
+                result = num1 * num2;
+                break;
+            case '/':
+                result = num1 / num2;
+                break;
+            case '%':
+                result = num1 % num2;
+                break;
+            case '^':
+                for(int i = 0; i < num2; i++) {
+                    result *= num1;
+                }
+                break;
+        }
+        System.out.println(num1 + " " + sign + " " + num2 + " = " + result);
+    }
+}
