f546b97b12aa2312281ca884788b8fe179174cac
EvgeniyaSharova <sharova.evgeniya8@gmail.com>
2023-03-09 16:42:42 +0300

    check

--- src/com/startjava/lesson_2_3_4/array/ArrayTheme.java
+++ src/com/startjava/lesson_2_3_4/array/ArrayTheme.java
@@ -7,11 +7,9 @@ public class ArrayTheme {
         System.out.println("1. Реверс значений массива");
         int[] numbersArr = {4, 1, 5, 2, 7, 3, 6};
         int length = numbersArr.length;
-        System.out.print("Массив до модификации :");
 
-        for (int i = 0; i < length; i++) {
-            System.out.print(" " + numbersArr[i]);
-        }
+        System.out.print("Массив до модификации :");
+        printNumbersArray(numbersArr);
 
         for (int i = 0; i < length / 2; i++) {
             int temp = numbersArr[i];
@@ -20,13 +18,12 @@ public class ArrayTheme {
         }
 
         System.out.print("\nМассив после модификации :");
+        printNumbersArray(numbersArr);
 
-        for (int i = 0; i < length; i++) {
-            System.out.print(" " + numbersArr[i]);
-        }
         System.out.println("\n\n2. Вывод произведения элементов массива");
         int[] numbers = new int[10];
         length = numbers.length;
+
         for (int i = 0; i < length; i++) {
             numbers[i] = i;
         }
@@ -41,30 +38,74 @@ public class ArrayTheme {
         System.out.println("\n3. Удаление элементов массива");
         double[] doubleArr = new double[15];
         length = doubleArr.length;
+
         for (int i = 0; i < length; i++) {
             doubleArr[i] = Math.random();
         }
+
         System.out.println("Исходный массив :");
         printDoubleArray(doubleArr);
+
         int count = 0;
         double middleCell = doubleArr[length / 2];
+
         for (int i = 0; i < length; i++) {
             if (doubleArr[i] > middleCell) {
                 doubleArr[i] = 0;
                 count++;
             }
         }
+
         System.out.println("\nИзмененный массив :");
         printDoubleArray(doubleArr);
+
         System.out.println("\nКоличество обнуленных ячеек " + count);
 
+        System.out.println("\n4. Вывод элементов массива лесенкой в обратном порядке");
+        char[] charsArr = new char[26];
+        length = charsArr.length;
+        char capitalLetter = 'Z';
 
+        for (int i = 0; i < length; i++) {
+            charsArr[i] = capitalLetter;
+            for (int j = 0; j <= i; j++) {
+                System.out.print(charsArr[j]);
+            }
+            System.out.println();
+            capitalLetter--;
+        }
+
+        System.out.println("\n5. Генерация уникальных чисел");
+        numbers = new int[30];
+        length = numbers.length;
+        for (int i = 0; i < length; i++) {
+            numbers[i] = (int) (Math.random() * 40 + 60);
+        }
+        int value = 0;
+        for (int i = 0; i < length; i++) {
+            value = numbers[i];
+            for (int j = 0; j < length; j++) {
+                if (value == numbers[j]) {
+                    numbers[j] = (int) (Math.random() * 40 + 60);
+                }
+
+            }
+        }
+        Arrays.sort(numbers);
+        System.out.println(Arrays.toString(numbers));
     }
 
-    private static void printDoubleArray(double[] doubleArr) {
-        int length = doubleArr.length;
+    private static void printNumbersArray(int[] array) {
+        int length = array.length;
         for (int i = 0; i < length; i++) {
-            System.out.printf("%.3f ", doubleArr[i]);
+            System.out.print(" " + array[i]);
+        }
+    }
+
+    private static void printDoubleArray(double[] array) {
+        int length = array.length;
+        for (int i = 0; i < length; i++) {
+            System.out.printf("%.3f ", array[i]);
             if (i == 7) {
                 System.out.println();
             }
