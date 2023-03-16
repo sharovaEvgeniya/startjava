6dbaa41dc4c7c8d2889392a3204bc61932229f99
EvgeniyaSharova <sharova.evgeniya8@gmail.com>
2023-03-16 17:32:43 +0300

    check

--- src/com/startjava/lesson_2_3_4/calculator/Calculator.java
+++ src/com/startjava/lesson_2_3_4/calculator/Calculator.java
@@ -1,30 +1,42 @@
 public class Calculator {
-    int num1;
-    int num2;
-    char sign;
-    public double calculate(String str) {
-        mathematicalExpressionArray(str);
-        switch (sign) {
-            case '+':
-                return Math.addExact(num1, num2);
-            case '-':
-                return Math.subtractExact(num1, num2);
-            case '*':
-                return Math.multiplyExact(num1, num2);
-            case '/':
-                return Math.floorDiv(num1, num2);
-            case '%':
-                return Math.floorMod(num1, num2);
-            case '^':
-                return Math.pow(num1, num2);
+    private static int num1;
+    private static int num2;
+    private static char sign;
+    private static double result;
+
+    public static double calculate(String expression) {
+        parsExpressionArray(expression);
+        if (num1 < 0 || num2 < 0) {
+            System.out.println("Введите положительные числа");
+            return result = 0;
         }
-        return 0;
+        return result = switch (sign) {
+            case '+' -> Math.addExact(num1, num2);
+            case '-' -> Math.subtractExact(num1, num2);
+            case '*' -> Math.multiplyExact(num1, num2);
+            case '/' -> Math.floorDiv(num1, num2);
+            case '%' -> Math.floorMod(num1, num2);
+            case '^' -> Math.pow(num1, num2);
+            default -> 0;
+        };
     }
 
-    private void mathematicalExpressionArray(String str) {
-        String[] mathematicalExpressionArray = str.split(" ");
-        num1 = Integer.parseInt(mathematicalExpressionArray[0]);
-        num2 = Integer.parseInt(mathematicalExpressionArray[2]);
-        sign = mathematicalExpressionArray[1].charAt(0);
+    public static void outputsResult() {
+        if (result % 1 == 0) {
+            System.out.println("Результат : " + (int) result);
+        } else {
+            System.out.printf("Результат : %.3f", result);
+        }
+    }
+
+    private static void parsExpressionArray(String expression) {
+        String[] partsExpression = expression.split(" ");
+        try {
+            num1 = Integer.parseInt(partsExpression[0]);
+            num2 = Integer.parseInt(partsExpression[2]);
+            sign = partsExpression[1].charAt(0);
+        } catch (RuntimeException e) {
+            System.out.println("Введите корректные значения");
+        }
     }
 }
--- src/com/startjava/lesson_2_3_4/calculator/CalculatorTest.java
+++ src/com/startjava/lesson_2_3_4/calculator/CalculatorTest.java
@@ -2,17 +2,12 @@ import java.util.Scanner;
 
 public class CalculatorTest {
     public static void main(String[] args) {
-        Calculator calculator = new Calculator();
         Scanner scanner = new Scanner(System.in);
         String answer = "yes";
         do {
             System.out.print("Введите математическое выражение: ");
-            double result = calculator.calculate(scanner.nextLine());
-            if (result % 1 == 0) {
-                System.out.println("Результат : " + (int) result);
-            } else {
-                System.out.printf("Результат : %.3f", result);
-            }
+            Calculator.calculate(scanner.nextLine());
+            Calculator.outputsResult();
             System.out.println("Хотите продолжить вычисления? [yes/no]: ");
             answer = scanner.nextLine();
         } while (!answer.equals("no"));
